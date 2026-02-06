8a644b625c25af01742367be00b62e1118d19554
Larissa <82511298+larissa-devweb@users.noreply.github.com>
2026-02-06 12:19:21 -0300

    cabeçalho do .c

--- mandelbrot.h
+++ mandelbrot.h
@@ -1,1 +1,26 @@
+#ifndef MANDELBROT_H
+#define MANDELBROT_H
 
+#include <stdint.h>
+
+typedef struct {
+    int largura;
+    int altura;
+    int max_iteracoes;
+    double x_min;
+    double x_max;
+    double y_min;
+    double y_max;
+    uint8_t* pixels; // array de pixels
+
+} DadosMandelbrot;
+
+//calculo
+
+DadosMandelbrot* calcular_mandelbrot (  int largura, int altura,  double x_min, double x_max,double y_min,
+                    double y_max; int max_iteracoes);
+
+// liberar memória
+void liberar_dados_mandelbrot(DadosMandelbrot* dados);
+
+#endif
