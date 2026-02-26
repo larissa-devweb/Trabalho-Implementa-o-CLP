8a0bfd0688f856b51f3c8020ccf66728bd42907d
Larissa <82511298+larissa-devweb@users.noreply.github.com>
2026-02-26 09:42:03 -0300

    Refactor aplicar_cores_avancadas function signature
    Removed redundant parameters from aplicar_cores_avancadas function declaration.

--- mandelbrot.h
+++ mandelbrot.h
@@ -19,10 +19,7 @@ typedef struct {
 DadosMandelbrot* calcular_mandelbrot (  int largura, int altura,  double x_min, double x_max,double y_min,
                     double y_max, int max_iteracoes);
 
-
-
-void aplicar_cores_avancadas(uint8_t* pixels, int* iteracoes, 
-                             int largura, int altura, int max_iter);
+void aplicar_cores_avancadas(uint8_t* pixels, int* iteracoes, double* radios, int largura, int altura, int max_iter);
 
 // Função para liberar memória
 void liberar_dados_mandelbrot(DadosMandelbrot* dados);
